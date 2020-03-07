97db580ac5bcb96fb7d28061e2a29a306f4d1b73
irinarykunova <iri.rykunova@gmail.com>
2020-03-07 22:23:20 +0100

    isFeminineNoun

---
+++ src/ExerciseIntro.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ExerciseIntro {
+
+   public static void main(String[] args){
+       Scanner scan = new Scanner(System.in);
+       String noun = scan.next();
+       System.out.println("> " + isFeminineNoun(noun));
+   }
+
+   public static boolean isFeminineNoun(String noun){
+       return noun.endsWith("e");
+   }
+}
